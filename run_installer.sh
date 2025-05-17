3fa47956e749139399494e205e03a420f47e9503
Jarrod Doyle <thejarroddoyle@gmail.com>
2025-05-17 22:50:46 +0100

    Update run script

--- run_installer.sh
+++ run_installer.sh
@@ -4,6 +4,6 @@ export WINEPREFIX=/stuff/Games/thief/
 export WINE=/home/jarrod/.local/share/lutris/runners/wine/lutris-GE-Proton8-10-x86_64/bin/wine64
 export WINEARCH=win64
 
-EXECUTABLE='Build/RoguePatcher_1.28.0.exe'
+EXECUTABLE='Build/RoguePatcher_1.28.1.exe'
 
 $WINE "$EXECUTABLE" "$@"
